c1cd04b587af74df1d4ebb106179aecc332a0ca4
Ross Morey <ross.morey@gmail.com>
2016-09-15 10:34:36 -0700

    refactored game.js

--- js/game.js
+++ js/game.js
@@ -1,33 +1,44 @@
 class BingoGame {
   constructor(questions, answers) {
-    this.shuffledAnswers = this.shuffledAs(answers);
-    this.shuffledQuestions = this.shuffledQs(questions);
+    this.shuffledAnswers = this.shuffledQAs(answers, "answer");
+    this.shuffledQuestions = this.shuffledQAs(questions, "question");
     this.currentQuestionIndex = 0;
     this.currentQuestion = () => (
       this.shuffledQuestions[this.currentQuestionIndex]
     );
   }
 
-  modCurrentQuestion() {
-    this.currentQuestion().key = "token";
+  shuffledQAs(qAndAs, label) {
+    let result = [];
+
+    for (var i = 1; i <= 25; i++) {
+      let qA = {key: i};
+      qA[label] = qAndAs[i];
+      result.push(qA);
+    }
+
+    return this.shuffle(result);
   }
 
-  nextQuestionIndex() {
-    this.currentQuestionIndex += 1;
-    if (this.shuffledQuestions.length ===
-      this.currentQuestionIndex) {
-        this.currentQuestionIndex = 0;
+  // Shuffle helper method (http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript)
+  shuffle(arr) {
+    let result = arr.slice(0);
+
+    var j, x, i;
+    for (var i = result.length; i; i--) {
+      j = Math.floor(Math.random() * i);
+      x = result[i - 1];
+      result[i - 1] = result[j];
+      result[j] = x;
     }
-    // Error if all tiles are "token" -- this will never happen though in the real game.
-    if (this.currentQuestion().key === "token") {
-      this.nextQuestionIndex();
-    }
+
+    return result;
   }
 
   guess(index) {
     if (this.currentQuestion().key === this.shuffledAnswers[index].key) {
       this.shuffledAnswers[index].key = "token";
-      this.modCurrentQuestion();
+      this.markCurrentQuestion();
       this.nextQuestionIndex();
       return true;
     } else {
@@ -35,103 +46,62 @@ class BingoGame {
     }
   }
 
-  shuffledQs(questions) {
-    let result = [];
+  nextQuestionIndex() {
+    this.currentQuestionIndex += 1;
 
-    for (var i = 1; i <= 25; i++) {
-      result.push({
-        key: i,
-        question: questions[i]
-      });
+    if (this.shuffledQuestions.length ===
+      this.currentQuestionIndex) {
+        this.currentQuestionIndex = 0;
     }
 
-    this.shuffle(result);
-    return result;
-  }
-
-  // Shuffle helper method (http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript)
-  shuffle(arr) {
-    var j, x, i;
-    for (var i = arr.length; i; i--) {
-      j = Math.floor(Math.random() * i);
-      x = arr[i - 1];
-      arr[i - 1] = arr[j];
-      arr[j] = x;
+    if (this.currentQuestion().key === "token") {
+      this.nextQuestionIndex();
     }
   }
 
-  shuffledAs(answers) {
-    let result = [];
-
-    for (var i = 1; i <= 25; i++) {
-      result.push({key: i, answer: answers[i]});
-    }
-
-    this.shuffle(result);
-    return result;
+  markCurrentQuestion() {
+    this.currentQuestion().key = "token";
   }
-
 
   isWon() {
-    let won, segment;
+    let segment;
+    let won = false;
 
+    let checkSegment = () => (
+      segment.every((el) => (
+        el.key==="token"
+      ))
+    );
+
+    // checks horizontal rows
     for (let i=0; i<=20; i+=5) {
       segment = this.shuffledAnswers.slice(i, i+5);
-
-      won = segment.every((el) => (
-        el.key==="token"
-      ));
-
-      if (won) return true;
+      if (checkSegment()) won = true;
     }
 
+    //checks vertical columns
     for (let j=0; j<5; j++) {
       segment = [];
       for (let m=0; m<25; m++) {
-        if (m%5===j) {
-          segment.push(this.shuffledAnswers[m]);
-        }
+        if (m%5===j) segment.push(this.shuffledAnswers[m]);
       }
-
-      won = segment.every((el) => (
-        el.key==="token"
-      ));
-
-      if (won) return true;
+      if (checkSegment()) won = true;
     }
 
-
-    segment = [
-      this.shuffledAnswers[4],
-      this.shuffledAnswers[8],
-      this.shuffledAnswers[12],
-      this.shuffledAnswers[16],
-      this.shuffledAnswers[20]
+    //checks diagonals
+    const diagCounts = [
+      [4,8,12,16,20],
+      [0,6,12,18,24]
     ];
 
-    won = segment.every((el) => (
-      el.key==="token"
-    ));
+    diagCounts.forEach((diagCount) => {
+      segment = diagCount.map((count) =>
+        (this.shuffledAnswers[count])
+      );
+      if (checkSegment()) won = true;
+    });
 
-    if (won) return true;
-
-
-    segment = [
-      this.shuffledAnswers[0],
-      this.shuffledAnswers[6],
-      this.shuffledAnswers[12],
-      this.shuffledAnswers[18],
-      this.shuffledAnswers[24]
-    ];
-
-    won = segment.every((el) => (
-      el.key==="token"
-    ));
-
-    if (won) return true;
-
-
-    return false;
+    return won;
   }
 }
 
